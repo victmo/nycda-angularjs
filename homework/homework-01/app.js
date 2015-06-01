0039ba13eee57f9d7bd1af9dcd67f875d1af28b3
victmo <victmo@gmail.com>
2015-06-01 01:46:09 -0400

    Homework 01 extras

--- homework/homework-01/app.js
+++ homework/homework-01/app.js
@@ -27,5 +27,26 @@ myModule.controller('ThemeCtrl', function(){
 		{folder: 'Trip', files: ['Beach.jpg','Bar.jpg','Friends.jpg','Hotel reservation.txt']}
 	];
 
+	self.allExpanded = false;
+	
+	self.expand = function(item){
+		item.expanded = !item.expanded;
+		self.allExpanded = areAllExpanded();
+	};
+
+	function areAllExpanded(){
+		for(var i=0, len=self.fileStructure.length; i<len; i++){
+			if(!self.fileStructure[i].expanded) return false;
+		}
+		return true;
+	}
+
+	self.toggleAll = function(){
+		self.allExpanded = !self.allExpanded;
+		for(var i=0, len=self.fileStructure.length; i<len; i++){
+			self.fileStructure[i].expanded = self.allExpanded;
+		}
+	};
+
 
 });
